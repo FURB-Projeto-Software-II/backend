2f30cb163cef186179c73d34641ad470abd7a3d0
Diogo Warmeling <diogo.warmeling@gmail.com>
2020-05-13 11:17:25 -0300

    remocao do .env do git

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
 const jwt = require('jsonwebtoken')
 const cors = require('cors')
-require('dotenv/config')
+require('dotenv').config()
 
 const app = express()
 
