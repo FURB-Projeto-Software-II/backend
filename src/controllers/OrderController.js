b4b6305a3aec89379e37591c1b96395b19fd9022
josehteixeira <joseehteixeira@gmail.com>
2020-07-05 21:42:37 -0300

    Ajuste na rota de marcar o pacote como recebido para que possa ser executada pelo cliente da ordem ou pelo armazém

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -92,7 +92,7 @@ exports.received = async (req, res) => {
     const order = await Order.findById(req.params.id)
 
     if (order == null) return res.status(402).send("Ordem nao encontrada")
-    if (order.id_client != req.userId) return res.status(401).send("Usuário inválido")
+    if (order.id_client != req.userId && order.id_storage != req.userId) return res.status(401).send("Usuário inválido")
     if (order.status == 1) return res.status(402).send("Ordem ja recebida")
 
     order.status = 1
